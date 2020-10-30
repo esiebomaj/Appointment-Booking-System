5510e805aaa83a629f7e275d14d7e343f42d9013
Lipton Zuma <solomonboateng231@gmail.com>
2020-10-30 09:09:37 +0000

    update

--- landingPage/app.js
+++ landingPage/app.js
@@ -3,6 +3,8 @@ let signIn = document.querySelector('#sign-inbtn')
 let signUp = document.querySelector('#sign-upbtn')
 let navLinks = document.querySelectorAll('.nav-link')
 let humburger = document.querySelector('.navbar-toggler')
+let main = document.querySelector('main')
+let upArrow = document.querySelector('.up')
 
 navLinks.forEach(navlink => navlink.addEventListener(('click'), () => {
     humburger.click()
@@ -18,7 +20,16 @@ window.addEventListener('resize', () => {
         signUp.innerHTML = 'Sign Up'
     }
 })
-window.addEventListener(('scroll'), () => {
-    let top = navbar.getBoundingClientRect().top
+window.addEventListener('scroll', () => {
+    let top = main.getBoundingClientRect().top
+    if (top > 282) {
+        upArrow.classList.remove('show')
+        upArrow.classList.add('hide')
+        console.log('work')
+    } else {
+        upArrow.classList.remove('hide')
+        upArrow.classList.add('show')
+        console.log('not-working')
+    }
 
 })
